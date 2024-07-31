bf76e6fe65a7e96e0572f4e4c79674e9c8463fef
kkarnesdesigns <40835120+kkarnesdesigns@users.noreply.github.com>
2024-07-31 19:10:31 -0400

    Update step-4-form.js

--- step-4-form.js
+++ step-4-form.js
@@ -3272,33 +3272,7 @@ const storageItemKey = "step-4-survey";
             questionElement.innerHTML = "";
             questionElement.appendChild(surveyRow);
         });
-        function saveSurveyData (survey) {
-    const data = survey.data;
-    data.pageNo = survey.currentPageNo;
-    window.localStorage.setItem(storageItemKey, JSON.stringify(data));
-}
-const survey = new Survey.Model(json);
-survey.onComplete.add((sender, options) => {
-    console.log(JSON.stringify(sender.data, null, 3));
-});
-// Save survey results to the local storage
-survey.onValueChanged.add(saveSurveyData);
-survey.onCurrentPageChanged.add(saveSurveyData);
-
-// Restore survey results
-const prevData = window.localStorage.getItem(storageItemKey) || null;
-if (prevData) {
-    const data = JSON.parse(prevData);
-    survey.data = data;
-    if (data.pageNo) {
-        survey.currentPageNo = data.pageNo;
-    }
-}
-
-// Empty the local storage after the survey is completed
-survey.onComplete.add(() => {
-    window.localStorage.setItem(storageItemKey, "");
-});
+        
        
 
       survey.render("surveyContainer");
